ce4fe5a0d139c9c574a358bb797633a735324e2b
yuchanandrew <anrh8554@colorado.edu>
2025-10-06 18:24:44 -0600

    Added build configurations to vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,12 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  // If the below doesn't work, remove
+  build: {
+    sourcemap: false,        // Speeds up builds
+    target: 'esnext',        // Optimize output for modern browsers
+    chunkSizeWarningLimit: 1000, // Optional, avoids noisy warnings
+  },
   server: {
     host: true,
     port: 5173,
