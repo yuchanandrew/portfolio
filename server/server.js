fa98335bd6345373ad0bf1e890df10675d5fbe72
yuchanandrew <anrh8554@colorado.edu>
2025-10-20 17:02:42 -0600

    Added additional CORS configuration

--- server/server.js
+++ server/server.js
@@ -13,6 +13,8 @@ const redirect_uri = "https://api.andrewrho.dev/callback";
 app.use(cors({
     origin: 'https://www.andrewrho.dev',
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
 }));
 
 const pool = mysql.createPool({
